b0f75693c04e2bc65e1293da2efd63b4f77ead62
adrija <adrijasingh13@gmail.com>
2023-04-30 23:40:02 +0530

    Added MongoDB connection and user registration functionality

---
+++ src/mongodb.js
@@ -0,0 +1,23 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/cpms_login_signup_db")
+.then (()=>{
+    console.log("mongodb connected ");
+})
+.catch(()=>{
+    console.log("failed to connect"); 
+})
+const LogInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection=new mongoose.model("Collection1",LogInSchema)
+
+module.exports=collection
